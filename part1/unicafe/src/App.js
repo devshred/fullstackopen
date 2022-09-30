9bd421c1c05045b536603d0df4082c35cf2d983b
Johannes Schmidt <devshred@tigerflanke.de>
2022-09-30 15:09:25 +0200

    1.11*: unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,7 +8,7 @@ const Button = ({name, onClickEvent}) => {
   )
 }
 
-const StatisticLine = ({text, value}) => <div>{text} {value}</div>
+const StatisticLine = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
@@ -19,14 +19,16 @@ const Statistics = ({good, neutral, bad}) => {
     const positive = <>{(good) / total * 100} %</>
   
     return (
-      <>
-        <StatisticLine text='good' value={good} />
-        <StatisticLine text='neutral' value={neutral} />
-        <StatisticLine text='bad' value={bad} />
-        <StatisticLine text='all' value={total} />
-        <StatisticLine text='average' value={average} />
-        <StatisticLine text='positive' value={positive} />
-      </>
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='all' value={total} />
+          <StatisticLine text='average' value={average} />
+          <StatisticLine text='positive' value={positive} />
+        </tbody>
+      </table>
     )
   }
 }
